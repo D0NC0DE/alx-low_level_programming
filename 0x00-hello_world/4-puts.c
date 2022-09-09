af1399eafd2563aca2ecec07addc0dd453cbfcc9
Winner_don <olusolawinner1@gmail.com>
2022-09-09 08:37:03 -0700

    update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  *main - entry point
+ *description: prints with puts
  *return: 0
  */
 
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
