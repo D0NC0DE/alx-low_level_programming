60fe822f35ce9da84b24cf064a15253eb232f3bf
Winner_don <olusolawinner1@gmail.com>
2022-09-17 08:45:44 -0700

    .2-print_alphabet_x10.c.swp

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
-  * print_alphabet_x10: function print alphabet
+  * print_alphabet_x10 - function print alphabet
   * Return: void
   */
 
 void print_alphabet_x10(void)
 {
-	char alpha = 'a';
 	int i = 0;
 
 	while (i < 10)
 	{
+		char alpha = 'a';
+
 		while (alpha <= 'z')
 		{
 			_putchar(alpha);
